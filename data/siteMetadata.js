380f10d9f1bbbf42078539404742b2afcab15319
skececi <sam.kececi@gmail.com>
2023-12-11 14:03:57 -0500

    analytics

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -14,18 +14,6 @@ const siteMetadata = {
   github: 'https://github.com/skececi',
   linkedin: 'https://www.linkedin.com/in/samkececi',
   locale: 'en-US',
-  analytics: {
-    // supports plausible, simpleAnalytics, umami or googleAnalytics
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    simpleAnalytics: false, // true or false
-    umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
-  },
-  newsletter: {
-    // supports mailchimp, buttondown, convertkit, klaviyo
-    // Please add your .env file and modify it according to your selection
-    provider: '',
-  },
 }
 
 module.exports = siteMetadata
